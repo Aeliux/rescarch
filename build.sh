bcd8e705ce315b39112460767b5f6632925ca6ed
aeliux <itsaeliux@gmail.com>
2026-01-08 00:00:16 +0330

    Fix loopback support

--- build.sh
+++ build.sh
@@ -41,9 +41,12 @@ prepare
 
 artifacts=()
 
+cmdline_common="archisobasedir=%INSTALL_DIR%"
+
 declare -A boot_vars=(
   ["RA_BOOT_SPLASH"]="quiet splash loglevel=3 rd.udev.log_level=3 vt.global_cursor_default=0"
-  ["RA_BOOT_CMDLINE"]="archisobasedir=%INSTALL_DIR% archisosearchuuid=%ARCHISO_UUID% cow_label=RA_DATA cow_nofail=y"
+  ["RA_BOOT_CMDLINE"]="${cmdline_common} archisosearchuuid=%ARCHISO_UUID% cow_label=RA_DATA cow_nofail=y"
+  ["RA_LOOPBACK_CMDLINE"]="${cmdline_common} img_dev=UUID=\${archiso_img_dev_uuid} img_loop=\"\${iso_path}\""
 )
 declare -A pacman_vars=(
   ["RA_REPOSITORY"]="file://$(realpath out/packages)"
